e560ee76cd73cfa99933069f5b0d42820ba6805e
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-09-10 21:40:39 +0400

    move the switch statement in the separate component

--- app/quiz/[id]/page.tsx
+++ app/quiz/[id]/page.tsx
@@ -6,10 +6,11 @@ import { getQuiz } from "@/storage/quizzes";
 import { Quiz } from "@/storage/types";
 import { Loading } from "./components/Loading";
 import { NotPublished } from "./components/NotPublished";
-import { Header } from "./components/Header";
-import { Question } from "./components/Question";
-import { Button } from "./components/Button";
-import { Footer } from "./components/Footer";
+import { Header } from "./components/BlockRenderer/components/Header";
+import { Question } from "./components/BlockRenderer/components/Question";
+import { Button } from "./components/BlockRenderer/components/Button";
+import { Footer } from "./components/BlockRenderer/components/Footer";
+import { BlockRenderer } from "./components/BlockRenderer/BlockRenderer";
 
 export default function QuizRenderPage() {
   const params = useParams();
@@ -34,52 +35,15 @@ export default function QuizRenderPage() {
   }
 
   if (!quiz.published) {
-    return NotPublished({ quiz });
+    return <NotPublished quiz={quiz} />;
   }
 
   return (
     <div className="p-6 max-w-3xl mx-auto space-y-6">
       <h1 className="text-2xl font-bold">{quiz.title}</h1>
-
-      {quiz.blocks.map((block) => {
-        switch (block.type) {
-          case "header": {
-            const text = String(block.props?.text ?? "");
-            return <Header key={block.id} id={block.id} text={text} />;
-          }
-
-          case "question": {
-            const qType =
-              (block.props?.type as "single" | "multi" | "text") ?? "single";
-            const question = String(block.props?.question ?? "");
-            const options = Array.isArray(block.props?.options)
-              ? block.props.options
-              : [];
-
-            return (
-              <Question
-                id={block.id}
-                question={question}
-                options={options}
-                qType={qType}
-              />
-            );
-          }
-
-          case "button": {
-            const text = String(block.props?.text ?? "Next");
-            return <Button id={block.id} text={text} />;
-          }
-
-          case "footer": {
-            const text = String(block.props?.text ?? "");
-            return <Footer id={block.id} text={text} />;
-          }
-
-          default:
-            return null;
-        }
-      })}
+      {quiz.blocks.map((block) => (
+        <BlockRenderer key={block.id} block={block} />
+      ))}
     </div>
   );
 }
